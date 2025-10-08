7349d2443ed589a6544b0e43e3af2e4559637535
Evgeniia Kay <kaygorodovaevg94@gmail.com>
2025-10-08 16:30:50 +0300

    second commit popup

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -2,8 +2,25 @@ const modal = document.getElementById('subscribe-modal');
 const closeButton = document.querySelector('.modal__close_times');
 const modalContent = document.querySelector('.modal__content');
 
+function setCookie(name, value, days = 365) {
+  let expires = "";
+  if (days) {
+    let date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
 function showModal() {
-  const isClosed = localStorage.getItem('modalClosed');
+  const isClosed = getCookie('modalClosed');
   if (isClosed !== 'true') {
     modal.classList.add('modal_active');
     
@@ -26,9 +43,9 @@ function showModal() {
 function closeModal() {
   modal.classList.remove('modal_active');
   try {
-    localStorage.setItem('modalClosed', 'true');
+    setCookie('modalClosed', 'true', 365);
   } catch (error) {
-    console.error('Ошибка сохранения в localStorage:', error);
+    console.error('Ошибка установки cookie:', error);
   }
 }
 
